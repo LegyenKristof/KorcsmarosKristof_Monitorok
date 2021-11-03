dc7925e15be3da36c5d58110a597d8b4ac606760
Kristof <you@example.com>
2021-11-04 00:43:56 +0100

    Bootstrap kinézet kész

--- index.php
+++ index.php
@@ -34,48 +34,56 @@ $monitorok = Monitor::beolvas();
     <title>Document</title>
     <link rel="stylesheet" href="main.css">
     <script src="main.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </head>
-<body>
+<body class="bg-primary bg-gradient bg-opacity-25">
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-2">
+            <form method="POST">
+            <div>
+                <span>Név: </span><br><input type="text" name="nev" id="nev" required>
+            </div>
+            <div>
+                <span>Gyártó: </span><br><input type="text" name="gyarto" id="gyarto" required>
+            </div>
+            <div>
+                <span>Képfrissítési frekvencia: </span><br><input type="number" name="kepfrissites" id="kepfrissites" required>
+            </div>
+            <div>
+                <span>Ár: </span><br><input type="number" name="ar" id="ar" required>
+            </div>
+            <div>
+                <span>Gyártva: </span><br><input type="date" name="gyartasideje">
+            </div>
+            <div>
+                <span></span><input type="submit" value="Hozzáadás" id="hozzaad">
+            </div>
+        </form>
+        </div>
+        <div class="col-lg-10">
+            <table class="table table-striped">
+                <tr><th>Név</th><th>Gyártó</th><th>Képfrissítési frekvencia</th><th>Ár</th><th>Gyártva</th><th></th><th></th></tr>
+                <?php
 
-<form method="POST">
-    <div>
-        <span>Név: </span><input type="text" name="nev" id="nev" required>
-    </div>
-    <div>
-        <span>Gyártó: </span><input type="text" name="gyarto" id="gyarto" required>
-    </div>
-    <div>
-        <span>Képfrissítési frekvencia: </span><input type="number" name="kepfrissites" id="kepfrissites" required>
-    </div>
-    <div>
-        <span>Ár: </span><input type="number" name="ar" id="ar" required>
-    </div>
-    <div>
-        <span>Gyártva: </span><input type="date" name="gyartasideje">
-    </div>
-    <div>
-        <span></span><input type="submit" value="Hozzáadás" id="hozzaad">
-    </div>
-</form>
-    
-<ul>
-<?php
+                foreach($monitorok as $monitor){
+                    echo "<tr>";
+                    //echo "ID: " . $monitor -> getId() . "<br>";
+                    echo "<td>" . $monitor -> getNev() . "</td>";
+                    echo "<td>" . $monitor -> getGyarto() . "</td>";
+                    echo "<td>" . $monitor -> getKepfrissites() . "</td>";
+                    echo "<td>" . $monitor -> getAr() . "</td>";
+                    echo "<td>" . $monitor -> getGyartasideje() -> format("Y-m-d") . "</td>";                    
+                    echo "<td><a href='szerkeszt.php?id=" . $monitor -> getId() . "'><button>Szerkesztés</button></a></td>";
+                    echo "<td><form method='POST' class='formButton'><button name='torles' value='" . $monitor -> getId() . "'>Törlés</button></form></td>";
+                    echo "</tr>";
+                }
 
-foreach($monitorok as $monitor){
-    echo "<li>";
-    //echo "ID: " . $monitor -> getId() . "<br>";
-    echo "Név: " . $monitor -> getNev() . "<br>";
-    echo "Gyártó: " . $monitor -> getGyarto() . "<br>";
-    echo "Képfrissítési frekvencia: " . $monitor -> getKepfrissites() . "<br>";
-    echo "Ár: " . $monitor -> getAr() . "<br>";
-    echo "Gyártva: " . $monitor -> getGyartasideje() -> format("Y-m-d") . "<br>";
-    echo "<form method='POST' class='formButton'><button name='torles' value='" . $monitor -> getId() . "'>Törlés</button></form>";
-    echo "<a href='szerkeszt.php?id=" . $monitor -> getId() . "'><button>Szerkesztés</button></a>";
-    echo "</li>";
-}
-
-?>
-</ul>
-
+                ?>
+            </table>
+        </div>
+    </div>
+</div>
 </body>
 </html>
--- szerkeszt.php
+++ szerkeszt.php
@@ -29,29 +29,40 @@ if ($_SERVER["REQUEST_METHOD"] === "POST"){
     <title>Document</title>
     <link rel="stylesheet" href="main.css">
     <script src="szerkeszt.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </head>
-<body>
+<body class="bg-primary bg-gradient bg-opacity-25">
 
-<form method="POST">
-    <div>
-        <span>Név: </span><input type="text" name="nev" id="nev" value="<?php echo $monitor -> getNev() ?>" required>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-2">
+            <form method="POST">
+                <div>
+                    <span>Név: </span><br><input type="text" name="nev" id="nev" value="<?php echo $monitor -> getNev() ?>" required>
+                </div>
+                <div>
+                    <span>Gyártó: </span><br><input type="text" name="gyarto" id="gyarto" value="<?php echo $monitor -> getGyarto() ?>" required>
+                </div>
+                <div>
+                    <span>Képfrissítési frekvencia: </span><br><input type="number" name="kepfrissites" id="kepfrissites" value="<?php echo $monitor -> getKepfrissites() ?>" required>
+                </div>
+                <div>
+                    <span>Ár: </span><br><input type="number" name="ar" id="ar" value="<?php echo $monitor -> getAr() ?>" required>
+                </div>
+                <div>
+                    <span>Gyártva: </span><br><input type="date" name="gyartasideje" value="<?php echo $monitor -> getGyartasideje() -> format("Y-m-d") ?>">
+                </div> 
+                <div>
+                    <span></span><input type="submit" value="Szerkesztés" id="szerkesztes">        
+                </div>   
+            </form>
+        </div>
+        <div class="col-lg-10"> 
+
+        </div>
     </div>
-    <div>
-        <span>Gyártó: </span><input type="text" name="gyarto" id="gyarto" value="<?php echo $monitor -> getGyarto() ?>" required>
-    </div>
-    <div>
-        <span>Képfrissítési frekvencia: </span><input type="number" name="kepfrissites" id="kepfrissites" value="<?php echo $monitor -> getKepfrissites() ?>" required>
-    </div>
-    <div>
-        <span>Ár: </span><input type="number" name="ar" id="ar" value="<?php echo $monitor -> getAr() ?>" required>
-    </div>
-    <div>
-        <span>Gyártva: </span><input type="date" name="gyartasideje" value="<?php echo $monitor -> getGyartasideje() -> format("Y-m-d") ?>">
-    </div> 
-    <div>
-        <span></span><input type="submit" value="Szerkesztés" id="szerkesztes">        
-    </div>   
-</form>
+</div>
 
 </body>
 </html>
